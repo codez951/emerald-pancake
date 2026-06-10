2666499c3abe728831dbca1fdfeb960733655358
cute Z <aryansatyam.mainacc@gmail.com>
2026-06-11 00:00:13 +0530

    Add Working Player Controller

---
+++ scripts/player_controller.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+
+var direction: Vector2 = Vector2.ZERO
+var player_form: String = "square"
+var player_shape: CharacterBody2D
+var triangle: PackedScene = preload("res://scenes/triangle.tscn")
+var square: PackedScene = preload("res://scenes/square.tscn")
+
+
+func _ready() -> void:
+	form_loader()
+
+
+func _physics_process(delta: float) -> void:
+	direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("character_up"):
+		direction += Vector2(0, -1)
+	if Input.is_action_pressed("character_down"):
+		direction += Vector2(0, 1)
+	if Input.is_action_pressed("character_right"):
+		direction += Vector2(1, 0)
+	if Input.is_action_pressed("character_left"):
+		direction += Vector2(-1, 0)
+	if Input.is_action_just_pressed("fire"):
+		print("Fire")
+		
+	player_shape.move_to(direction)
+
+
+func _process(delta: float) -> void:
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	var turret_pos: Vector2 = player_shape.get_node("Turret").global_position
+	var error: Vector2 = mouse_pos - turret_pos
+	var angle: float = atan2(error.y, error.x)
+	
+	player_shape.get_node("Turret").turret_movement(angle)
+
+
+func form_loader():
+	if player_form == "square":
+		player_shape = square.instantiate()
+	if player_form == "triangle":
+		player_shape = triangle.instantiate()
+		
+	add_child(player_shape)
--- scripts/triangle.gd
+++ scripts/triangle.gd
@@ -3,31 +3,11 @@ extends CharacterBody2D
 
 var direction: Vector2 = Vector2.ZERO
 var speed: float = 500
+var hp: float
+var attack: float
 
 
-func _process(delta: float) -> void:
-	var mouse_pos: Vector2 = get_global_mouse_position()
-	var turret_pos: Vector2 = $Turret.global_position
-	var error: Vector2 = mouse_pos - turret_pos
-	var angle: float = atan2(error.y, error.x)
-	
-	$Turret.turret_movement(angle)
-
-
-func _physics_process(delta: float) -> void:
-	direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("character_up"):
-		direction += Vector2(0, -1)
-	if Input.is_action_pressed("character_down"):
-		direction += Vector2(0, 1)
-	if Input.is_action_pressed("character_right"):
-		direction += Vector2(1, 0)
-	if Input.is_action_pressed("character_left"):
-		direction += Vector2(-1, 0)
-	if Input.is_action_just_pressed("fire"):
-		print("Fire")
-	
-	direction = direction.normalized()
-	velocity = speed * direction
+func move_to(dir: Vector2):
+	dir = dir.normalized()
+	velocity = speed * dir
 	move_and_slide()
